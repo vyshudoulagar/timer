40a28d0fce9210d05bc1a5b2dbe2c36d92b738f1
WishNavy <du.vyshu@gmail.com>
2024-02-16 14:29:41 -0800

    created timer

--- timer1.js
+++ timer1.js
@@ -1,10 +1,10 @@
 const timer = process.argv.slice(2);
 
-for(const time of timer){
-    if(time !== Number || time === -Number){
+for (const time of timer) {
+    if (time < 0 || isNaN(time)) {
         return;
     }
     setTimeout(() => {
         process.stdout.write('\x07');
-    }, time*1000)
+    }, time * 1000)
 }
